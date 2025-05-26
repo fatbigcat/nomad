400bff6d55fd74001efba5707dfa84068ac24b5c
fatbigcat <xmissalina@gmail.com>
2025-05-26 10:48:32 +0200

    rotated API key because I'm silly

--- nomad/app/firebaseConfig.ts
+++ nomad/app/firebaseConfig.ts
@@ -3,13 +3,15 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 import { getFirestore } from "firebase/firestore";
+// Make sure to install and configure 'react-native-dotenv' or use process.env if supported
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY as string;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC6Fqi_s8hCmKf4PmLez2aHQJbZrDBNn7Q",
+  apiKey: GOOGLE_MAPS_API_KEY,
   authDomain: "nomad-832ea.firebaseapp.com",
   projectId: "nomad-832ea",
   storageBucket: "nomad-832ea.appspot.com",
